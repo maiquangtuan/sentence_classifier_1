d84feba0d19048e5a9780c1d72303af42c8153a8
ilndht <fizzganktem@gmail.com>
2023-02-01 22:55:32 +0700

    download.py

---
+++ download.py
@@ -0,0 +1,9 @@
+# In this file, we define download_model
+# It runs during container build time to get model weights built into the container
+
+# In this example: A Huggingface BERT model
+
+
+
+if __name__ == "__main__":
+    pass
