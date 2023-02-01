4b3ef748679f29698895d1ba7775dbf133a810b1
ilndht <fizzganktem@gmail.com>
2023-02-01 22:46:55 +0700

    update app.py

--- app.py
+++ app.py
@@ -1,28 +1,37 @@
 from transformers import pipeline
 import torch
 import numpy as np
-
+import onnxruntime
+from transformers import AutoTokenizer
+import numpy as np
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
-    global model
+    global tokenizer, session
     
-    device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
-
+    tokenizer = AutoTokenizer.from_pretrained("model_tokenizer")
+    model_path = "mount2_model_classify_sentence_1.onnx"
+    session = onnxruntime.InferenceSession(model_path)
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
 def inference(model_inputs:dict) -> dict:
-    global model
-    for key in model_inputs.keys(): 
-        model_inputs[key]= model_inputs[key].astype(np.int64)
-    # Parse out your arguments
-    prompt = model_inputs.get('prompt', None)
-    if prompt == None:
-        return {'message': "No prompt provided"}
+    global tokenizer,session
+
     
+    # Parse out your arguments
+    inputs = tokenizer(
+        model_inputs,
+        padding=True,
+        truncation=True,
+        return_attention_mask=True,
+        return_token_type_ids=True,
+        return_tensors="np",
+    )
+    for key in inputs.keys(): 
+        inputs[key]= inputs[key].astype(np.int64)
     # Run the model
-    result = model(prompt)
+    onnx_preds = session.run(None, dict(inputs))[0]
+    result = dict(zip(model_inputs, onnx_preds))
 
     # Return the results as a dictionary
     return result
