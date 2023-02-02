1fd1f1c3841120ac9f9fa30209bfd0645483b2db
ilndht <66417297+ilndht@users.noreply.github.com>
2023-02-03 02:49:23 +1100

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ import numpy as np
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
-    global tokenizer, session
+    global tokenizer, model
     
     tokenizer = AutoTokenizer.from_pretrained("model_tokenizer")
     model_path = "mount2_model_classify_sentence_1.onnx"
-    session = onnxruntime.InferenceSession(model_path)
+    model = onnxruntime.InferenceSession(model_path)
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
 def inference(model_inputs:dict) -> dict:
-    global tokenizer,session
+    global tokenizer, model
 
     model_input = model_inputs.get('input', None)
     if  model_input == None:
@@ -32,7 +32,7 @@ def inference(model_inputs:dict) -> dict:
     for key in inputs.keys(): 
         inputs[key]= inputs[key].astype(np.int64)
     # Run the model
-    onnx_preds = session.run(None, dict(inputs))[0]
+    onnx_preds = model.run(None, dict(inputs))[0]
     result = dict(zip(model_input, onnx_preds))
 
     # Return the results as a dictionary
